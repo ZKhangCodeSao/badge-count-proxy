6d3a112f9fe6a87d1e5946c88547bba6a20246c2
Phan Hoàng Khang <121412318+ZKhangCodeSao@users.noreply.github.com>
2026-07-23 09:47:06 +0700

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,10 @@ app.get("/badges/:userId", async (req, res) => {
 				headers: {
 					"User-Agent":
 						"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
-					Accept: "application/json",
+					Accept: "application/json, text/plain, */*",
+					"Accept-Language": "en-US,en;q=0.9",
+					Referer: "https://www.roblox.com/",
+					Origin: "https://www.roblox.com",
 				},
 			});
 
